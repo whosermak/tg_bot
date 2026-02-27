b1068c9d35462cffdf3ee98a0cea6264e9f91d8b
Rend <whosermak@gmail.com>
2026-02-28 01:36:38 +1000

    инит тг бота

--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,25 @@
-from dotenv import load_dotenv
 import os
+import asyncio
+from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
 
-load_dotenv()  
-
+load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
-print(TOKEN) 
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command(commands=["start"]))
+async def start(message: types.Message):
+    await message.reply("Стартовое сообщение")
+
+@dp.message()
+async def echo(message: types.Message):
+    await message.reply(f"Ты написал: {message.text}")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
